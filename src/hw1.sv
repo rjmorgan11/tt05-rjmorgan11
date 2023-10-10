73eec25dabd1f27551bef906748ad7e518bc98c8
Rylan Morgan <101641101+rjmorgan11@users.noreply.github.com>
2023-10-09 20:19:34 -0400

    Update hw1.sv

--- src/hw1.sv
+++ src/hw1.sv
@@ -1,4 +1,4 @@
-`default_nettype none
+//`default_nettype none
 
 module tt_um_rjmorgan11_calculator_chip #( parameter MAX_COUNT = 24'd10_000_000 )(
     input  wire [7:0] ui_in,    // Dedicated inputs
