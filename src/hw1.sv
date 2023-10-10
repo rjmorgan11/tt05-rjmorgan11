6f4ab62c499dfeb1dc316bfdc850bb3de3105c49
Rylan Morgan <101641101+rjmorgan11@users.noreply.github.com>
2023-10-09 20:18:17 -0400

    Update hw1.sv

--- src/hw1.sv
+++ src/hw1.sv
@@ -3,10 +3,10 @@
 module tt_um_rjmorgan11_calculator_chip #( parameter MAX_COUNT = 24'd10_000_000 )(
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
+    //input  wire [7:0] uio_in,   // IOs: Input path
+    //output wire [7:0] uio_out,  // IOs: Output path
+    //output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    //input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
