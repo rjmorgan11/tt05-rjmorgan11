3f0290b71dd9b81ac84ecee1dc8c9dd4b7eeba01
rjmorgan11 <rjmorgan@andrew.cmu.edu>
2023-10-30 20:24:16 -0400

    lots of changes

--- src/calc.sv
+++ src/calc.sv
@@ -10,23 +10,104 @@ module tt_um_rjmorgan11_calculator_chip #( parameter MAX_COUNT = 24'd10_000_000 
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-// Wouldn't the buttons be active low on a real design?
-    logic [7:0] state;
-    assign uo_out = (ena) ? state : 8'h00;
-    assign uio_out = ui_in;
-    assign uio_oe = 8'hff;
-    
+    logic [7:0] state, last_state, tmp;
+    logic [2:0] flags;
+    logic overflow, neg, zero;
+    assign uo_out = state;
+    assign uio_out[7:5] = flags;
+    assign uio_oe = 8'hE0;
+    assign flags = {overflow, neg, zero};
 
-  always_ff @(posedge clk, posedge rst_n) begin
-        if (rst_n) begin
+    always_ff @(posedge clk, negedge rst_n) begin
+        if (~rst_n) begin
             state <= 8'd0;
+            overflow <= 0;
+            neg <= 0;
+            zero <= 0;
+    
         end
         else if (uio_in[0]) begin
-            case(uio_in[2:1])
-                2'b00: state <= state + ui_in;
-                2'b01: state <= state - ui_in;
-                2'b10: state <= state | ui_in;
-                2'b11: state <= (state == ui_in) ? 1 : 0;
+            last_state <= state;
+            overflow <= 0;
+            neg <= 0;
+            zero <= 0;
+    
+            case(uio_in[4:1])
+                4'h0: begin
+                    state <= state + ui_in; // add
+                    zero <= (state + ui_in) == 8'h00;
+                    tmp = state + ui_in;
+                    overflow <= tmp[7] != last_state[7];
+                    neg <= tmp[7];
+                end
+                4'h1: begin
+                    state <= state - ui_in; // subtract
+                    zero <= (state - ui_in) == 8'h00;
+                    tmp = state - ui_in;
+                    overflow <= tmp[7] != last_state[7];
+                    neg <= tmp[7];
+                end
+                4'h2: begin
+                    state <= state | ui_in; // or
+                    zero <= (state | ui_in) == 8'h00;
+            
+                end
+                4'h3: begin
+                    state <= state & ui_in; // and
+                    zero <= (state & ui_in) == 8'h00;
+            
+                end
+                4'h4: begin
+                    state <= state ^ ui_in; // xor
+                    zero <= (state ^ ui_in) == 8'h00;
+            
+                end
+                4'h5: begin
+                    state <= state << 1;    // left shift 1
+                    zero <= (state << 1) == 8'h00;
+            
+                end
+                4'h6: begin
+                    state <= state >> 1;    // right shift 1 logically
+                    zero <= (state >> ui_in) == 8'h00;
+            
+                end
+                4'h7: begin
+                    state <= state[7] | (state >> 1); // right shift 1 artithmetically
+                    zero <= (state[7] | (state >> 1)) == 8'h00;
+                    neg <= state[7];
+                end
+                4'h8: begin
+                    state <= ~state + 8'h01; // negate
+                    zero <= (~state + 8'h01) == 8'h00;
+                    tmp = ~state + 8'h01;
+                    overflow <= tmp[7] != last_state[7];
+                    neg <= tmp[7];
+                end
+                4'h9: begin
+                    state <= ~state;        // invert
+                    zero <= (~state) == 8'h00;
+            
+                end
+                4'ha: begin
+                    state <= {state[0], state[1],
+                              state[2], state[3],
+                              state[4], state[5],
+                              state[6], state[7]}; // reverse bit pattern
+            
+                end
+                // 4'hb: // not used
+                // 4'hc: // not used
+                4'hd: begin
+                    state <= state > ui_in; // input less than (unsigned)
+                end
+                4'he: begin
+                    state <= state < ui_in; // input greater than (unsigned)
+                end
+                4'hf: begin
+                    state <= (state == ui_in); // equal to (active low)
+                    zero <= state == ui_in;
+                end
             endcase
         end
     end
---
+++ src/calc_tb.sv
@@ -0,0 +1,198 @@
+
+module calc_tb;
+    logic [7:0] ui_in;    // Dedicated inputs
+    logic [7:0] uo_out;   // Dedicated outputs
+    logic [7:0] uio_in;   // IOs: Input path
+    logic [7:0] uio_out;  // IOs: output path
+    logic [7:0] uio_oe;   // IOs: Enable path (active high: 0=input, 1=output)
+    logic       ena;      // will go high when the design is enabled
+    logic       clk;      // clock
+    logic       rst_n;     // reset_n - low to reset
+
+    tt_um_rjmorgan11_calculator_chip DUT (.*);
+    initial begin
+        clk = 0;
+        #5 clk = 1;
+        forever #5 clk = ~clk;
+    end
+    task do_reset;
+        rst_n = 0;
+        @(posedge clk);
+        rst_n <= 1;
+        @(posedge clk);
+    endtask: do_reset
+
+    task send_add(logic [7:0] uin);
+        ui_in <= uin;
+        uio_in[4:1] <= 8'h00;
+        @(posedge clk);
+        uio_in[0] <= 1;
+        @(posedge clk);
+        
+        uio_in[0] <= 0;
+        @(posedge clk);
+    endtask: send_add
+
+    task send_sub(logic [7:0] uin);
+        ui_in <= uin;
+        uio_in[4:1] <= 8'h01;
+        @(posedge clk);
+        uio_in[0] <= 1;
+        @(posedge clk);
+        
+        uio_in[0] <= 0;
+        @(posedge clk);
+    endtask: send_sub
+
+    task send_or(logic [7:0] uin);
+        ui_in <= uin;
+        uio_in[4:1] <= 8'h02;
+        @(posedge clk);
+        uio_in[0] <= 1;
+        @(posedge clk);
+        
+        uio_in[0] <= 0;
+        @(posedge clk);
+    endtask: send_or
+
+    task send_and(logic [7:0] uin);
+        ui_in <= uin;
+        uio_in[4:1] <= 8'h03;
+        @(posedge clk);
+        uio_in[0] <= 1;
+        @(posedge clk);
+        
+        uio_in[0] <= 0;
+        @(posedge clk);
+    endtask: send_and
+
+    task send_xor(logic [7:0] uin);
+        ui_in <= uin;
+        uio_in[4:1] <= 8'h04;
+        @(posedge clk);
+        uio_in[0] <= 1;
+        @(posedge clk);
+        
+        uio_in[0] <= 0;
+        @(posedge clk);
+    endtask: send_xor
+
+    task send_lft_shft;
+        uio_in[4:1] <= 8'h05;
+        @(posedge clk);
+        uio_in[0] <= 1;
+        @(posedge clk);
+        
+        uio_in[0] <= 0;
+        @(posedge clk);
+    endtask: send_lft_shft
+
+    task send_rt_shft;
+        uio_in[4:1] <= 8'h06;
+        @(posedge clk);
+        uio_in[0] <= 1;
+        @(posedge clk);
+        
+        uio_in[0] <= 0;
+        @(posedge clk);
+    endtask: send_rt_shft
+
+    task send_rt_arith_shft;
+        uio_in[4:1] <= 8'h07;
+        @(posedge clk);
+        uio_in[0] <= 1;
+        @(posedge clk);
+        
+        uio_in[0] <= 0;
+        @(posedge clk);
+    endtask: send_rt_arith_shft
+
+    task send_neg;
+        uio_in[4:1] <= 8'h08;
+        @(posedge clk);
+        uio_in[0] <= 1;
+        @(posedge clk);
+        
+        uio_in[0] <= 0;
+        @(posedge clk);
+    endtask: send_neg
+
+    task send_inv;
+        uio_in[4:1] <= 8'h09;
+        @(posedge clk);
+        uio_in[0] <= 1;
+        @(posedge clk);
+        
+        uio_in[0] <= 0;
+        @(posedge clk);
+    endtask: send_inv
+
+    task send_rev;
+        uio_in[4:1] <= 8'h0a;
+        @(posedge clk);
+        uio_in[0] <= 1;
+        @(posedge clk);
+        
+        uio_in[0] <= 0;
+        @(posedge clk);
+    endtask: send_rev
+
+    task send_lsthn(logic [7:0] uin);
+        ui_in <= uin;
+        uio_in[4:1] <= 8'h0d;
+        @(posedge clk);
+        uio_in[0] <= 1;
+        @(posedge clk);
+        
+        uio_in[0] <= 0;
+        @(posedge clk);
+    endtask: send_lsthn
+
+    task send_grtthn(logic [7:0] uin);
+        ui_in <= uin;
+        uio_in[4:1] <= 8'h0e;
+        @(posedge clk);
+        uio_in[0] <= 1;
+        @(posedge clk);
+        
+        uio_in[0] <= 0;
+        @(posedge clk);
+    endtask: send_grtthn
+
+    task send_equ(logic [7:0] uin);
+        ui_in <= uin;
+        uio_in[4:1] <= 8'h0f;
+        @(posedge clk);
+        uio_in[0] <= 1;
+        @(posedge clk);
+        
+        uio_in[0] <= 0;
+        @(posedge clk);
+    endtask: send_equ
+
+    initial begin
+        $monitor("op = 0x%x, in = 0x%x (0b%b), out = 0x%x(0b%b), flags = %b", uio_in[4:1], ui_in, ui_in, uo_out, uo_out, uio_out[7:5]);
+        ena = 1;
+        do_reset();
+        send_add(8'hff);
+        send_sub(8'h0f);
+        send_or(8'h01);
+        send_and(8'h00);
+        send_xor(8'h55);
+        send_lft_shft();
+        send_rt_shft();
+        send_rt_arith_shft();
+        send_neg();
+        send_inv();
+        send_rev();
+        send_lsthn(8'h7f);
+        send_lsthn(8'hff);
+        send_grtthn(8'hff);
+        send_grtthn(8'h7f);
+        send_equ(8'h00);
+        $finish;
+    end
+
+
+
+endmodule: calc_tb
